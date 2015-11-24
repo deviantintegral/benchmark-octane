a69e0806b0e80773beb6cc0fe9ee22fc7c118ff0
Andrew Berry <deviantintegral@gmail.com>
2015-11-24 14:48:28 -0500

    Initial require() support for base, box2d and raytrace

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = require('./lib/octane.js');
+var Octane = require("./lib/octane/base.js");
+
+// Octane requires these classes to be globals.
+global.BenchmarkSuite = Octane.BenchmarkSuite;
+global.Benchmark = Octane.Benchmark;
+global.BenchmarkResult = Octane.BenchmarkResult;
+
+module.exports = {
+  Octane: Octane,
+  Box2D: require("./lib/octane/box2d.js"),
+  RayTrace: require("./lib/octane/raytrace.js"),
+};
